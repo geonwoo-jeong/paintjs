5ec98f5620b2c96ea511c97eaf14da16164f5731
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-04-22 13:45:08 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -45,6 +49,7 @@ const handleColorClick = event => {
   } = event;
 
   ctx.strokeStyle = backgroundColor;
+  ctx.fillStyle = backgroundColor;
 };
 
 const handleRangeChange = event => {
@@ -65,11 +70,18 @@ const handleModeClick = () => {
   }
 };
 
+const handleCanvasClick = () => {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+};
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 if (colors) {
